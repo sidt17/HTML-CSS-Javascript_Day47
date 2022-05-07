cc296b8cf291ce7a86b0a497b1545142910c8d9b
Siddhant <santoshshrivastava39@gmail.com>
2022-05-08 01:08:56 +0530

    for loop used to populate multiple information

--- Home.js
+++ Home.js
@@ -6,18 +6,25 @@ const createInnerHtml =() =>{
     const HeaderHtml ="<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
                       "<th>Salary</th><th>StartDate</th><th>Actions</th>";
 
-    const innerHtml = `${HeaderHtml}
-    <tr>
+    let innerHtml=`${HeaderHtml}`;
+     let empPayrollList= createEmployeePayRollJSON();
+     for(const empPayRollData of empPayrollList)
+     {
+
+     
+
+        innerHtml = `${innerHtml}
+    <tr> 
+
     <td><img class="profile" alt="" 
-        src="img1.jpg">
+        src=${empPayRollData._profilePic}>
      </td>
-     <td>Siddhant</td>
-     <td>Male</td>
-     <td><div class='dept-label'>HR</div>
-        <div class='dept-label'>Finance</div>
-     </td>
-     <td>400000</td>
-     <td>01 January 2021</td>
+     <td>${empPayRollData._name}</td>
+     <td>${empPayRollData._gender}</td>
+      <td>${getDeptHtml(empPayRollData._department)}
+      </td>
+     <td>${empPayRollData._salary}</td>
+     <td>${empPayRollData._startDate}</td>
      <td>
      <button id="1" onclick="remove(this)">Remove</button> 
     <button id="1" onclick="update(this)">Edit</button> 
@@ -25,5 +32,39 @@ const createInnerHtml =() =>{
     </td> 
 </tr>
 `;
+}
     document.querySelector('#display').innerHTML=innerHtml;
 }
+const getDeptHtml = (deptList) => {
+    let deptHtml ='';
+    for(const dept of deptList){
+        deptHtml=`${deptHtml}<div class='dept-label'>${dept}</div>`
+    }
+    return deptHtml;
+}
+    const createEmployeePayRollJSON=()=>{
+        let employeePayRollListLocal=[
+            {
+                _name:'Siddhant',
+                _gender:'Male',
+                _department:['Engineering','Finance'],
+                _salary:'50000',
+                _startDate:'29 Oct 2019',
+                _note:'',
+                _id:new Date().getTime(),
+                _profilePic:'img1.jpg'
+            },
+            {
+                _name:'Priya',
+                _gender:'Female',
+                _department:['Engineering','Finance'],
+                _salary:'40000',
+                _startDate:'1 Oct 2020',
+                _note:'',
+                _id:new Date().getTime(),
+                _profilePic:'img3.jpg'
+            }
+    
+        ];
+        return employeePayRollListLocal;
+    }
